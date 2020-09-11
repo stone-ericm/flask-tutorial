b6498525b7ffbf1b22bb3e2497e75799d0a19c74
Eric Stone <stone.ericm@gmail.com>
2020-09-11 16:57:04 -0400

    combined create functions into update.html

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -42,7 +42,7 @@ def create():
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/create.html')
+    return render_template('blog/update.html', post=None)
 
 
 def get_post(id, check_author=True):
