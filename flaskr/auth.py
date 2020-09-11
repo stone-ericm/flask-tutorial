24bddee909fad7e032afd0fa509355895bfe1fcf
Eric Stone <stone.ericm@gmail.com>
2020-09-11 16:35:58 -0400

    stay logged in when registering

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -23,7 +23,7 @@ def register():
         elif not password:
             error = 'Password is required.'
         elif db.execute(
-                'SELECT id FROM user WHERE username = ?', (username,)
+            'SELECT id FROM user WHERE username = ?', (username,)
         ).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
         if error is None:
@@ -32,7 +32,12 @@ def register():
                 (username, generate_password_hash(password))
             )
             db.commit()
-            return redirect(url_for('auth.login'))
+            user = db.execute(
+                'SELECT * FROM user WHERE username = ?', (username,)
+            ).fetchone()
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('blog.index'))
         flash(error)
     return render_template('register.html')
 
